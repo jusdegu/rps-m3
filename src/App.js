37277ec4776b270e1c3c4aedcbe86826866b1899
unknown <Justin>
2019-05-09 11:41:24 -0400

    Updated formatting. MVP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import logo from "./img/himpp.png";
 import rock from "./img/darock.jpg";
 import paper from "./img/paper.png";
@@ -17,14 +17,14 @@ import "./App.css";
 class GetName extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = { value: "" };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   handleSubmit(event) {
@@ -37,8 +37,12 @@ class GetName extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name: 
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -46,8 +50,7 @@ class GetName extends React.Component {
   }
 }
 
-function ButtonComponent (props) {
-
+function ButtonComponent(props) {
   function myClick(value) {
     // alert("You've selected " + value + "!");
     props.handleClick(value);
@@ -55,33 +58,47 @@ function ButtonComponent (props) {
 
   return (
     <div class="rowwide">
-      <div><h2>Welcome {props.p1Name}!</h2></div>
-      <div><h3>Click a button.</h3></div>
+      <div>
+        <h2>Welcome {props.p1Name}!</h2>
+      </div>
+      <div>
+        <h3>Click a button.</h3>
+      </div>
       <div class="rowslol">
-        <div class="button" onClick={() => myClick("Rock")}><img src={rock} className="img-button" alt="rock" /></div>
-        <div class="button" onClick={() => myClick("Paper")}><img src={paper} className="img-button" alt="paper" /></div>
-        <div class="button" onClick={() => myClick("Scissors")}><img src={scissors} className="img-button" alt="scissors" /></div>
+        <div class="button" onClick={() => myClick("Rock")}>
+          <img src={rock} className="img-button" alt="rock" />
+        </div>
+        <div class="button" onClick={() => myClick("Paper")}>
+          <img src={paper} className="img-button" alt="paper" />
+        </div>
+        <div class="button" onClick={() => myClick("Scissors")}>
+          <img src={scissors} className="img-button" alt="scissors" />
+        </div>
       </div>
     </div>
   );
-
-};
+}
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-    this.state = { submitted: false, p1Choice: "", p1Name: "", selection: false };
+    this.state = {
+      submitted: false,
+      p1Choice: "",
+      p1Name: "",
+      selection: false
+    };
     this.pcChoice = "";
   }
 
-  handleClick = (value) => {
+  handleClick = value => {
     this.setState({ p1Choice: value, selection: true });
   };
 
-  handleName = (name) => {
+  handleName = name => {
     this.setState({ p1Name: name, submitted: true });
   };
-  
+
   randomNum = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -89,13 +106,11 @@ class App extends Component {
 
     console.log("Random number is: " + ran);
 
-    if (ran === 1){
+    if (ran === 1) {
       return "Rock";
-    }
-    else if (ran === 2) {
+    } else if (ran === 2) {
       return "Paper";
-    }
-    else {
+    } else {
       return "Scissors";
     }
   };
@@ -103,25 +118,29 @@ class App extends Component {
   // Rock, Paper, Scissors
   analyze = () => {
     this.pcChoice = this.randomNum(1, 4);
-    if ((this.state.p1Choice === "Rock" && this.pcChoice === "Scissors") || 
-    (this.state.p1Choice === "Paper" && this.pcChoice === "Rock") ||
-    (this.state.p1Choice === "Scissors" && this.pcChoice === "Paper"))
+    if (
+      (this.state.p1Choice === "Rock" && this.pcChoice === "Scissors") ||
+      (this.state.p1Choice === "Paper" && this.pcChoice === "Rock") ||
+      (this.state.p1Choice === "Scissors" && this.pcChoice === "Paper")
+    )
       return 1;
-    else if (this.state.p1Choice === this.pcChoice)
-      return 3;
-    else
-      return 2;
-  }
+    else if (this.state.p1Choice === this.pcChoice) return 3;
+    else return 2;
+  };
 
   render() {
     let event;
     let result;
 
     if (this.state.p1Name === "") {
-      event = <GetName name="get-user-name" handleName={this.handleName}/>;
-    }
-    else {
-      event = <ButtonComponent p1Name={this.state.p1Name} handleClick={this.handleClick}/>;
+      event = <GetName name="get-user-name" handleName={this.handleName} />;
+    } else {
+      event = (
+        <ButtonComponent
+          p1Name={this.state.p1Name}
+          handleClick={this.handleClick}
+        />
+      );
     }
 
     if (this.state.selection === true) {
@@ -130,25 +149,43 @@ class App extends Component {
       console.log("Result: " + playerWon);
 
       if (playerWon === 1) {
-        result = <div><h1>You win!</h1> <h2>You selected {this.state.p1Choice}. PC selected {this.pcChoice}</h2></div>;
-      }
-      else if (playerWon === 2) {
-        result = <div><h1>You lose!</h1> <h2>You selected {this.state.p1Choice}. PC selected {this.pcChoice}</h2></div>;
-      }
-      else {
-        result = <div><h1>The game is tied!</h1> <h2>You selected {this.state.p1Choice}. PC selected {this.pcChoice}</h2></div>;
+        result = (
+          <div>
+            <h1>You win!</h1>{" "}
+            <h2>
+              You selected {this.state.p1Choice}. PC selected {this.pcChoice}
+            </h2>
+          </div>
+        );
+      } else if (playerWon === 2) {
+        result = (
+          <div>
+            <h1>You lose!</h1>{" "}
+            <h2>
+              You selected {this.state.p1Choice}. PC selected {this.pcChoice}
+            </h2>
+          </div>
+        );
+      } else {
+        result = (
+          <div>
+            <h1>The game is tied!</h1>{" "}
+            <h2>
+              You selected {this.state.p1Choice}. PC selected {this.pcChoice}
+            </h2>
+          </div>
+        );
       }
     }
 
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="spawn of satan" />
-        {event}
-        {result}
-      </header>
-    </div>
-    
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="spawn of satan" />
+          {event}
+          {result}
+        </header>
+      </div>
     );
   }
 }
